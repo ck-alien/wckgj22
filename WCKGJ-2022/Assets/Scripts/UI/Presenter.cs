b0cffc290660f330598e554f7e4dccf6e01d6e3b
Pilgyeong Gwak <pid011@kakao.com>
2022-03-16 18:47:00 +0900

    Add sprite resources

---
+++ WCKGJ-2022/Assets/Scripts/UI/Presenter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace EarthIsMine.UI
+{
+    public class Presenter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+    }
+}
