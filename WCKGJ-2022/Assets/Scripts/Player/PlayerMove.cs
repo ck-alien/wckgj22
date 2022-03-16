c0e6d206f87afa868644d33b7ec8699252d8cb90
happyuron <87974802+happyuron@users.noreply.github.com>
2022-03-17 00:21:24 +0900

    Change prefeb of player

--- WCKGJ-2022/Assets/Scripts/Player/PlayerMove.cs
+++ WCKGJ-2022/Assets/Scripts/Player/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EarthIsMine.Manager;
 
 namespace EarthIsMine.Player
 {
@@ -17,15 +18,11 @@ namespace EarthIsMine.Player
                 tr = GetComponent<Transform>();
             if (controller == null)
                 controller = GetComponent<PlayerController>();
-
         }
 
-
-
-
-        public void MoveOn(float _direction)
+        public void MoveRight(float _direction)
         {
-            tr.position += new Vector3(tr.position.x + _direction, tr.position.y, tr.position.z);
+            tr.position = new Vector3(tr.position.x + (_direction * controller.Speed * Time.deltaTime), tr.position.y, tr.position.z);
         }
 
     }
