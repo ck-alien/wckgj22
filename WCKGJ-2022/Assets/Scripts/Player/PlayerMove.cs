170c6ac9422f4d2663311e39e839c96102259e6d
happyuron <87974802+happyuron@users.noreply.github.com>
2022-03-16 22:16:51 +0900

    Make Basic PlayerMove System

--- WCKGJ-2022/Assets/Scripts/Player/PlayerMove.cs
+++ WCKGJ-2022/Assets/Scripts/Player/PlayerMove.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace EarthIsMine
+namespace EarthIsMine.Player
 {
     public class PlayerMove : MonoBehaviour
     {
+        private Transform tr;
+        private PlayerController controller;
 
+
+
+        private void Awake()
+        {
+            if (tr == null)
+                tr = GetComponent<Transform>();
+            if (controller == null)
+                controller = GetComponent<PlayerController>();
+
+        }
+
+
+
+
+        public void MoveOn(float _direction)
+        {
+            tr.position += new Vector3(tr.position.x + _direction, tr.position.y, tr.position.z);
+        }
 
     }
 }
