c0ed8300955867bc53a542c930da3623b64bcbd8
happyuron <87974802+happyuron@users.noreply.github.com>
2022-03-17 19:28:24 +0900

    Create Scripts

---
+++ WCKGJ-2022/Assets/Scripts/Object/Projectile/Projectile.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EarthIsMine
+{
+    public class Projectile<T> : MonoBehaviour
+    {
+
+
+    }
+}
---
+++ WCKGJ-2022/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EarthIsMine.Player
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+        GameObject bullet = Resources.Load<GameObject>("");
+
+
+    }
+}
